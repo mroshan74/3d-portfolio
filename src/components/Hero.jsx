3bfa0bb2703f7e3e3f84fa6ccb009378099dec16
Muhammed Roshan <mroshan74@gmail.com>
2023-03-06 22:46:27 +0530

    typedjs typewriting animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,41 @@
 import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  // Create reference to store the DOM element containing the animation
+	const el = useRef(null);
+  // Create reference to store the Typed instance itself
+	const typed = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        "design stuff",
+        "am passionate on coding",
+        "writes content",
+        "love open source",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+      loopCount: Infinity,
+      cursorChar: '_',
+    };
+    
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+    
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    }
+  }, [])
+
   return (
     <section className={`relative w-full h-[1024px] mx-auto`}>
       <div
@@ -19,8 +51,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Muhammed Roshan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I <span ref={el}/>
+            <br className='sm:block hidden' />
           </p>
         </div>
       </div>
